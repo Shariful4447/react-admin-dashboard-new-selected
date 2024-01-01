c5cbb21e1ae7b7a0944825344e5f205495af35f5
Shariful4447 <shariful.islam4447@gmail.com>
2024-01-01 09:23:13 +0100

    sidebar change

--- src/constants/sidebarMenu.js
+++ src/constants/sidebarMenu.js
@@ -1,14 +1,14 @@
-import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+// import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
 import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
-import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
-import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+// import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+// import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
 import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+// import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 
 export const sidebarMenu = [
     {
@@ -54,28 +54,28 @@ export const sidebarMenu = [
         icon: HelpOutlineOutlinedIcon,
         path: '/faq',
     },
-    {
-        title: 'Charts',
-        tag: 'divider'
-    },
-    {
-        title: 'Bar Chart',
-        icon: BarChartOutlinedIcon,
-        path: '/bar',
-    },
-    {
-        title: 'Pie Chart',
-        icon: PieChartOutlineOutlinedIcon,
-        path: '/pie',
-    },
-    {
-        title: 'Line Chart',
-        icon: TimelineOutlinedIcon,
-        path: '/line',
-    },
-    {
-        title: 'Geography Chart',
-        icon: MapOutlinedIcon,
-        path: '/geography',
-    },
+    // {
+    //     title: 'Charts',
+    //     tag: 'divider'
+    // },
+    // {
+    //     title: 'Bar Chart',
+    //     icon: BarChartOutlinedIcon,
+    //     path: '/bar',
+    // },
+    // {
+    //     title: 'Pie Chart',
+    //     icon: PieChartOutlineOutlinedIcon,
+    //     path: '/pie',
+    // },
+    // {
+    //     title: 'Line Chart',
+    //     icon: TimelineOutlinedIcon,
+    //     path: '/line',
+    // },
+    // {
+    //     title: 'Geography Chart',
+    //     icon: MapOutlinedIcon,
+    //     path: '/geography',
+    // },
 ]
