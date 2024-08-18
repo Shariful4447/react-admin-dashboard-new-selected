22e1081ee10c7cb33d1a97cc756aaa426800aed3
Shariful4447 <66253663+Shariful4447@users.noreply.github.com>
2024-08-18 22:22:12 +0200

    Update Topbar.jsx

--- src/pages/global/Topbar.jsx
+++ src/pages/global/Topbar.jsx
@@ -19,7 +19,7 @@ const Topbar = () => {
   return (
     <Box display="flex" justifyContent="space-between" p={2}>
 
-      {/* 🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧 SEARCH BAR 🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧 */}
+      
       <Box
         display="flex"
         borderRadius="3px"
